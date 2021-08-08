5fe585d78bb2f71d17ce46f8c429c9fb221b91d5
thang1509 <lnthang1509@gmail.com>
2021-08-08 17:56:50 +0700

    fit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '../src/custom.scss'
+import './custom.scss'
 import reportWebVitals from './reportWebVitals';
 
 
