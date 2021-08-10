764611faf8c4e8d0ff6ae0464087f4b80cc00926
Thang <lnthang1509@gmail.com>
2021-08-10 11:24:27 +0700

    fits

--- src/Components/News/index.js
+++ src/Components/News/index.js
@@ -132,54 +132,7 @@ export default function News() {
   </div>
  
 </div>
- <div>
- <Nav tabs>
-   <NavItem>
-     <NavLink
-     type="button"
-       className={classnames({ active: activeTab === '1' })}
-       onClick={() => { toggle('1'); }}
-     >
-       Tab1
-     </NavLink>
-   </NavItem>
-   <NavItem>
-     <NavLink
-       className={classnames({ active: activeTab === '2' })}
-       onClick={() => { toggle('2'); }}
-     >
-       More Tabs
-     </NavLink>
-   </NavItem>
- </Nav>
- <TabContent activeTab={activeTab}>
-   <TabPane tabId="1">
-     <Row>
-       <Col sm="12">
-         <h4>Tab 1 Contents</h4>
-       </Col>
-     </Row>
-   </TabPane>
-   <TabPane tabId="2">
-     <Row>
-       <Col sm="6">
-         <Card body>
-           <CardTitle>Special Title Treatment</CardTitle>
-           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-           <Button>Go somewhere</Button>
-         </Card>
-       </Col>
-       <Col sm="6">
-         <Card body>
-           <CardTitle>Special Title Treatment</CardTitle>
-           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-           <Button>Go somewhere</Button>
-         </Card>
-       </Col>
-     </Row>
-   </TabPane>
- </TabContent>
-</div>
+
 </>
 
 
